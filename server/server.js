fae248908529235da200975bf2610894f926e4db
Amit Bhogle <abhogle17@gmail.com>
2020-10-13 23:12:14 -0600

    Proxy scaffolding complete, rendering 2 apps

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use('/products/:id', express.static(path.join(__dirname, '../public')));
+
+// app.use('/', createProxyMiddleware({
+//   target: `http://localhost:3002`,
+//   changeOrigin: true
+// }))
+
+// app.use('/', createProxyMiddleware({
+//   target: `http://localhost:3001`,
+//   changeOrigin: true
+// }))
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
